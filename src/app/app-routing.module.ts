8de6bdd429b907210018c76dc511f4a29efc539a
Akshay <akshay@futurewise.co.in>
2019-08-30 18:29:35 +0530

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './component/protect-component/AdviserComponent/dashboard/dashboard.component';
 import { LeftsidebarComponent } from './component/left-sidebar/leftsidebar/leftsidebar.component';
+import { SubscriptionComponent } from './component/protect-component/AdviserComponent/Subscriptions/subscription/subscription.component';
 
 
 const routes: Routes = [
@@ -13,6 +14,10 @@ const routes: Routes = [
       {
         path:'dashboard',
         component:DashboardComponent
+      },
+      {
+        path:'subscription',
+        component:SubscriptionComponent
       },
       {
         path:'',
