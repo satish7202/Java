b56f4a9bd2c6bade8a1905d0070fff0b0860bfd2
sunny-ui <sunny@futurewise.co.in>
2019-09-03 19:10:30 +0530

    addd

--- src/app/component/protect-component/AdviserComponent/Subscriptions/subscription/client-subscription/client-subscription.component.ts
+++ src/app/component/protect-component/AdviserComponent/Subscriptions/subscription/client-subscription/client-subscription.component.ts
@@ -1,5 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+
+export interface PeriodicElement {
+  name: string;
+  email: string;
+  num: number;
+  balance: string;
+}
+const ELEMENT_DATA: PeriodicElement[] = [
+  {name: 'Drasti Zaveri', email: 'drasti@futurewise.co.in', num: 9987455665, balance: 'Rs.20,000'},
+  {name: 'Ajaykumar', email: 'ronak.hindocha@futurewise.co.in', num: 9987455665, balance: '0'},
+  {name: 'Sarvesh Shinde', email: 'sarvesh@gmail.com', num: 9987455665, balance: 'Rs.7,000'},
+ 
+];
+
+
 @Component({
   selector: 'app-client-subscription',
   templateUrl: './client-subscription.component.html',
@@ -11,5 +26,6 @@ export class ClientSubscriptionComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  displayedColumns: string[] = ['name','email', 'num', 'balance'];
+  dataSource = ELEMENT_DATA;
 }
--- src/app/component/protect-component/AdviserComponent/Subscriptions/subscription/invoices-subscription/invoices-subscription.component.ts
+++ src/app/component/protect-component/AdviserComponent/Subscriptions/subscription/invoices-subscription/invoices-subscription.component.ts
@@ -1,5 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface PeriodicElement {
+  date:string;
+  invoicenum:string;
+  name: string;
+  email: string;
+  status:string;
+  duedate: string;
+  amt: string;
+  balance:string;
+}
+const ELEMENT_DATA: PeriodicElement[] = [
+  {date: '26/08/2019',invoicenum: 'INV-19-20-103001',name: 'Drasti Zaveri', email: 'drasti@futurewise.co.in', status: 'OVERDUE', duedate: '05/09/2019', amt: '05/09/2019', balance: 'Rs.20,000'},
+  {date: '28/08/2019',invoicenum: 'INV-19-20-103012',name: 'Ajaykumar', email: 'ronak.hindocha@futurewise.co.in', status: 'PAID', duedate: '05/09/2019', amt: '05/09/2019', balance: '0'},
+  {date: '29/08/2019',invoicenum: 'INV-19-20-103024',name: 'Sarvesh Shinde', email: 'sarvesh@gmail.com', status: 'SENT', duedate: '05/09/2019', amt: '05/09/2019', balance: 'Rs.7,000'},
+ 
+];
+
 @Component({
   selector: 'app-invoices-subscription',
   templateUrl: './invoices-subscription.component.html',
@@ -11,5 +28,7 @@ export class InvoicesSubscriptionComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  displayedColumns: string[] = ['checkbox','date','invoicenum','name','status','email', 'duedate', 'amt','balance'];
+  dataSource = ELEMENT_DATA;
+  
 }
