b897d50793e26c4f0bc1900380d30f95f47396ec
sunny-ui <sunny@futurewise.co.in>
2019-09-04 17:22:43 +0530

    add

--- src/app/component/protect-component/AdviserComponent/Subscriptions/subscription/documents-subscriptions/documents-subscriptions.component.ts
+++ src/app/component/protect-component/AdviserComponent/Subscriptions/subscription/documents-subscriptions/documents-subscriptions.component.ts
@@ -2,19 +2,17 @@ import { Component, OnInit } from '@angular/core';
 
 export interface PeriodicElement {
   name: string;
-  date:string;
-  invoicenum:string;
-  email: string;
+  docname:string;
+  plan:string;
+  servicename:string;
+  cdate:string;
+  sdate: string;
+  clientsign: string;
   status:string;
-  duedate: string;
-  amt: string;
-  balance:string;
 } 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {date: '26/08/2019',invoicenum: 'INV-19-20-103001',name: 'Drasti Zaveri', email: 'drasti@futurewise.co.in', status: 'OVERDUE', duedate: '05/09/2019', amt: '05/09/2019', balance: 'Rs.20,000'},
-  {date: '28/08/2019',invoicenum: 'INV-19-20-103012',name: 'Ajaykumar', email: 'ronak.hindocha@futurewise.co.in', status: 'PAID', duedate: '05/09/2019', amt: '05/09/2019', balance: '0'},
-  {date: '29/08/2019',invoicenum: 'INV-19-20-103024',name: 'Sarvesh Shinde', email: 'sarvesh@gmail.com', status: 'SENT', duedate: '05/09/2019', amt: '05/09/2019', balance: 'Rs.7,000'},
- 
+  {name: 'Neelam Singh',docname: 'Scope of work', plan: 'Starter plan', servicename: 'AUM Linked fee', cdate: '25/08/2019', sdate: '25/08/2019', clientsign: '25/08/2019',status:'READY TO SEND'},
+   
 ];
 @Component({
   selector: 'app-documents-subscriptions',  
@@ -27,6 +25,6 @@ export class DocumentsSubscriptionsComponent implements OnInit {
 
   ngOnInit() {
   }
-  displayedColumns: string[] = ['date','invoicenum','name','status','email', 'duedate', 'amt','balance'];
+  displayedColumns: string[] = ['name','docname','plan', 'servicename', 'cdate','sdate','clientsign','status','icons'];
   dataSource = ELEMENT_DATA;
 }
