d879ee69c67b572bba35ea60a3e456f2c97ae4e7
Akshay <akshay@futurewise.co.in>
2019-09-11 18:13:31 +0530

    add

---
+++ src/app/component/protect-component/AdviserComponent/Subscriptions/subscription/subscriptions-subscription/subscription-modifiy-fee-structure/subscription-modifiy-fee-structure.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-subscription-modifiy-fee-structure',
+  templateUrl: './subscription-modifiy-fee-structure.component.html',
+  styleUrls: ['./subscription-modifiy-fee-structure.component.scss']
+})
+export class SubscriptionModifiyFeeStructureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
