cf9b29d03f9a9fd5689a49692760cf5403b68401
sunny-ui <sunny@futurewise.co.in>
2019-09-17 18:51:38 +0530

    some changes in settings css and new

--- src/app/component/protect-component/AdviserComponent/Subscriptions/subscription/quotations-subscription/quotations-subscription.component.ts
+++ src/app/component/protect-component/AdviserComponent/Subscriptions/subscription/quotations-subscription/quotations-subscription.component.ts
@@ -1,5 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+export interface PeriodicElement {
+  name: string;
+  docname:string;
+  plan:string;
+  
+  cdate:string;
+  sdate: string;
+  clientsign: string;
+  status:string;
+} 
+const ELEMENT_DATA: PeriodicElement[] = [
+  {name: 'Neelam Singh',docname: 'Scope of work', plan: 'Starter plan',  cdate: '25/08/2019', sdate: '25/08/2019', clientsign: '25/08/2019',status:'READY TO SEND'},
+   
+];
 @Component({
   selector: 'app-quotations-subscription',
   templateUrl: './quotations-subscription.component.html',
@@ -11,5 +24,6 @@ export class QuotationsSubscriptionComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  displayedColumns: string[] = ['name', 'docname', 'plan', 'cdate','sdate','clientsign','status','icons'];
+  dataSource = ELEMENT_DATA;
 }
