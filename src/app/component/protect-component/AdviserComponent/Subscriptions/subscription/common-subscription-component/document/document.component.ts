d46664ab69562f7ddd5c454e7511787d525be01a
sunny-ui <sunny@futurewise.co.in>
2019-09-19 19:27:19 +0530

    NEW CHANGES

--- src/app/component/protect-component/AdviserComponent/Subscriptions/subscription/common-subscription-component/document/document.component.ts
+++ src/app/component/protect-component/AdviserComponent/Subscriptions/subscription/common-subscription-component/document/document.component.ts
@@ -1,5 +1,18 @@
 import { Component, OnInit,Input } from '@angular/core';
 import { UpperSliderComponent } from '../upper-slider/upper-slider.component';
+export interface PeriodicElement {
+  document: string;
+  plan: string;
+  service:string;
+  date: string;
+  sdate: string;
+  cdate: string;
+  status:string;
+ } 
+ const ELEMENT_DATA: PeriodicElement[] = [
+  {document: "Scope of work", plan: 'Starter plan',service:'AUM Linked fee', date: "25/08/2019", sdate: '25/08/2019',cdate: '25/08/2019',status:'READY TO SEND'},
+  
+]; 
 @Component({
   selector: 'app-document',
   templateUrl: './document.component.html',
@@ -12,5 +25,6 @@ export class DocumentComponent implements OnInit {
   ngOnInit() {
     
   }
-
+  displayedColumns: string[] = ['checkbox','document','plan','service', 'date', 'sdate','cdate','status','icons'];
+  dataSource = ELEMENT_DATA;
 }
--- src/app/component/protect-component/AdviserComponent/Subscriptions/subscription/common-subscription-component/invoice/invoice.component.ts
+++ src/app/component/protect-component/AdviserComponent/Subscriptions/subscription/common-subscription-component/invoice/invoice.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit, Input } from '@angular/core';
+export interface PeriodicElement {
+  document: string;
+  plan: string;
+  date: string;
+  sdate: string;
+  cdate: string;
+  status:string;
+ } 
+ const ELEMENT_DATA: PeriodicElement[] = [
+  {document: "Scope of work", plan: 'Starter plan', date: "25/08/2019", sdate: '25/08/2019',cdate: '25/08/2019',status:'READY TO SEND'},
+  
+];
 
 @Component({
   selector: 'app-invoice',
@@ -10,10 +22,12 @@ export class InvoiceComponent implements OnInit {
   constructor() { }
   @Input() invoiceData;
   ngOnInit() {
-    console.log(this.invoiceData,"dawfiasfuhusfui")
+  
   }
   Close()
   {
     // this.subRight.showSubscription=true;
   }
+  displayedColumns: string[] = ['checkbox','document','plan', 'date', 'sdate','cdate','status','icons'];
+  dataSource = ELEMENT_DATA;
 }
