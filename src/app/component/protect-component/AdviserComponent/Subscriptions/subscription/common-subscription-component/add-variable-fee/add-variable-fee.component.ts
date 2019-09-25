c76fa831291dcc3d75a4836e70600eb412e91202
devshyni <devshyni@futurewise.co.in>
2019-09-25 11:26:46 +0530

    variable fee htnl side bar popup

--- src/app/component/protect-component/AdviserComponent/Subscriptions/subscription/common-subscription-component/add-variable-fee/add-variable-fee.component.ts
+++ src/app/component/protect-component/AdviserComponent/Subscriptions/subscription/common-subscription-component/add-variable-fee/add-variable-fee.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { subscriptionInject } from '../../../subscription-inject.service';
 
 @Component({
   selector: 'app-add-variable-fee',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddVariableFeeComponent implements OnInit {
 
-  constructor() { }
+  constructor(public subInjectService:subscriptionInject) { }
 
   ngOnInit() {
   }
-
+  Close(state)
+  {
+    this.subInjectService.rightSliderData(state)
+  }
+  closeTab(state,value)
+  {
+    console.log(state)
+    this.subInjectService.rightSliderData(state)
+    this.subInjectService.closeSlider(value)
+  }
 }
