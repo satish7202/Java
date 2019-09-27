4f640849f43c85c26b52c6da9ef5b89862706f62
Gaurav <gaurav@futurewise.co.in>
2019-09-27 18:49:17 +0530

    Formatted Subscription Component
    Signed-off-by: Gaurav <gaurav@futurewise.co.in>

--- src/app/component/protect-component/AdviserComponent/Subscriptions/subscription/subscription.component.ts
+++ src/app/component/protect-component/AdviserComponent/Subscriptions/subscription/subscription.component.ts
@@ -1,82 +1,72 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from 'src/app/Data-service/event.service';
 import { trigger, state, style, transition, animate } from '@angular/animations';
-import { from } from 'rxjs';
 import { SubscriptionInject } from '../subscription-inject.service';
 @Component({
   selector: 'app-subscription',
   templateUrl: './subscription.component.html',
   styleUrls: ['./subscription.component.css'],
-  animations:[
-    trigger('openClose',[
-      state('open',style({
-        left:'40%'
+  animations: [
+    trigger('openClose', [
+      state('open', style({
+        left: '40%'
       })),
-      state('openHelp',style({
+      state('openHelp', style({
         // width:'35%'
       })),
-      state('close',style({
+      state('close', style({
         // width:'0%'
-        left:'100%'
+        left: '100%'
       })),
-      transition('close => open',[animate('0.3s')]),
-      transition('open => close',[animate('0.1s')]),
-      transition('close => openHelp',[animate('0.3s')]),
-      transition('openHelp => close',[animate('0.1s')])
+      transition('close => open', [animate('0.3s')]),
+      transition('open => close', [animate('0.1s')]),
+      transition('close => openHelp', [animate('0.3s')]),
+      transition('openHelp => close', [animate('0.1s')])
     ])
   ]
 })
 export class SubscriptionComponent implements OnInit {
+  constructor(private eventService: EventService, private subinject: SubscriptionInject) {
+    this.eventService.sidebarSubscribeData.subscribe(
+      data => this.getFileResponseDataAum(data)
+    );
+    this.eventService.tabChangeData.subscribe(
+      data => this.getTabChangeData(data)
+    );
+    this.subinject.rightSideBarData.subscribe(
+      data => this.getRightSliderData(data)
+    );
+  }
 
   currentState;
-  ngOnInit() {
-   this.currentState='close'
-  }
   subscriptionTab;
 
   selected: any;
-  constructor(private eventService:EventService,private subinject:SubscriptionInject) {
-    this.eventService.sidebarSubscribeData.subscribe(
-      data => this.getFileResponseDataAum(data)
-     )
-     this.eventService.tabChangeData.subscribe(
-      data => this.gettabChangeData(data)
-     )
-     this.subinject.rightSideBarData.subscribe(
-       data =>this.getRightSliderData(data)
-     )
-   }
-
-   getFileResponseDataAum(data){
-        console.log(data)
-        this.subscriptionTab=data;
-
-   }
-   gettabChangeData(data){
-     console.log(data)
-     this.selected=data;
+  ngOnInit() {
+    this.currentState = 'close';
   }
-  getRightSliderData(value)
-  {
-    this.currentState=value
-    console.log(value)
+
+  getFileResponseDataAum(data) {
+    this.subscriptionTab = data;
+  }
+  getTabChangeData(data) {
+    this.selected = data;
+  }
+  getRightSliderData(value) {
+    this.currentState = value;
   }
 
 
-  rightBar()
-  {
-    this.currentState='open';
+  /*rightBar() {
+    this.currentState = 'open';
   }
-  rightBarHelp()
-  {
-    this.currentState='openHelp';
-  }
-  tabClick(event)
-  {
-    console.log(event)
+  rightBarHelp() {
+    this.currentState = 'openHelp';
+  }*/
+  tabClick(event) {
     this.subscriptionTab = event.tab.textLabel;
   }
-  help(){
+  help() {
 
   }
 }
