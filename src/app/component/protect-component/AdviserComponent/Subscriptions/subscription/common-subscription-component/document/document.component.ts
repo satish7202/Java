eaa4cb2fb5aa0cdd779d0aecfa44f6961d2d6b03
Gaurav <gaurav@futurewise.co.in>
2019-09-27 18:25:20 +0530

    Resolved all tsLint errors

--- src/app/component/protect-component/AdviserComponent/Subscriptions/subscription/common-subscription-component/document/document.component.ts
+++ src/app/component/protect-component/AdviserComponent/Subscriptions/subscription/common-subscription-component/document/document.component.ts
@@ -1,26 +1,35 @@
-import { Component, OnInit,Input } from '@angular/core';
-import { UpperSliderComponent } from '../upper-slider/upper-slider.component';
-import { MatDialogRef, MatDialog } from '@angular/material';
-import { subscriptionInject } from '../../../subscription-inject.service';
-import { ConfirmDialogComponent } from 'src/app/component/protect-component/common-component/confirm-dialog/confirm-dialog.component';
-import { EventService } from 'src/app/Data-service/event.service';
-import { SubscriptionPopupComponent } from '../subscription-popup/subscription-popup.component';
-import { SubscriptionService } from '../../../subscription.service';
-import * as _ from "lodash";
+import {Component, OnInit, Input} from '@angular/core';
+import {MatDialogRef, MatDialog} from '@angular/material';
+import {subscriptionInject} from '../../../subscription-inject.service';
+import {ConfirmDialogComponent} from 'src/app/component/protect-component/common-component/confirm-dialog/confirm-dialog.component';
+import {EventService} from 'src/app/Data-service/event.service';
+import {SubscriptionPopupComponent} from '../subscription-popup/subscription-popup.component';
+import {SubscriptionService} from '../../../subscription.service';
+import * as _ from 'lodash';
 
 export interface PeriodicElement {
   document: string;
   plan: string;
-  service:string;
+  service: string;
   date: string;
   sdate: string;
   cdate: string;
-  status:string;
- } 
- const ELEMENT_DATA: PeriodicElement[] = [
-  {document: "Scope of work", plan: 'Starter plan',service:'AUM Linked fee', date: "25/08/2019", sdate: '25/08/2019',cdate: '25/08/2019',status:'READY TO SEND'},
-  
+  status: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {
+    document: 'Scope of work',
+    plan: 'Starter plan',
+    service: 'AUM Linked fee',
+    date: '25/08/2019',
+    sdate: '25/08/2019',
+    cdate: '25/08/2019',
+    status: 'READY TO SEND'
+  },
+
 ];
+
 @Component({
   selector: 'app-document',
   templateUrl: './document.component.html',
@@ -28,121 +37,125 @@ export interface PeriodicElement {
 })
 export class DocumentComponent implements OnInit {
 
-  documentDesign;           
-  planDocumentData;
-  mappedData=[];
-  constructor(public dialogRef:MatDialogRef<DocumentComponent>,public subInjectService:subscriptionInject,private eventService:EventService,public dialog: MatDialog,private subService:SubscriptionService) 
-  {
+  constructor(public dialogRef: MatDialogRef<DocumentComponent>, public subInjectService: subscriptionInject,
+              private eventService: EventService, public dialog: MatDialog, private subService: SubscriptionService) {
     this.subInjectService.rightSliderDocument.subscribe(
-      data=>this.getDocumentsDesignData(data)
-    )
-   }
-  @Input() componentFlag:string;
+      data => this.getDocumentsDesignData(data)
+    );
+  }
+
+  documentDesign;
+  planDocumentData;
+  mappedData = [];
+
+  @Input() componentFlag: string;
+
+  displayedColumns: string[] = ['checkbox', 'document', 'plan', 'service', 'date', 'sdate', 'cdate', 'status', 'icons'];
+  dataSource = ELEMENT_DATA;
+
   ngOnInit() {
     this.getplanDocumentData();
-    this.documentDesign='true'
+    this.documentDesign = 'true';
   }
-  openPopup(data){
-    let Fragmentdata = {
+
+  openPopup(data) {
+    const Fragmentdata = {
       Flag: data,
-    }
+    };
     const dialogRef = this.dialog.open(SubscriptionPopupComponent, {
       width: '70%',
-      height:'100%',
+      height: '100%',
       data: Fragmentdata,
-      autoFocus:false,
-     
-   });
-   dialogRef.afterClosed().subscribe(result => {
+      autoFocus: false,
 
-  });
+    });
+    dialogRef.afterClosed().subscribe(result => {
+
+    });
   }
-  dialogClose(){
+
+  dialogClose() {
     this.dialogRef.close();
   }
-  selectDocument(data)
-  {
-    (data.selected)?this.unmapDocumentToPlan(data):this.mapDocumentToPlan(data);
+
+  selectDocument(data) {
+    (data.selected) ? this.unmapDocumentToPlan(data) : this.mapDocumentToPlan(data);
   }
-  mapDocumentToPlan(data)
-  {
-    data.selected=true;
-    this.mappedData.push(data)
-    console.log(this.mappedData.length)
+
+  mapDocumentToPlan(data) {
+    data.selected = true;
+    this.mappedData.push(data);
+    console.log(this.mappedData.length);
   }
-  unmapDocumentToPlan(data)
-  {
-    data.selected=false;
-    _.remove(this.mappedData,function(delData)
-    {
-      return  delData.documentRepositoryId ==data.documentRepositoryId;
-    })
-    console.log(this.mappedData.length)
+
+  unmapDocumentToPlan(data) {
+    data.selected = false;
+    _.remove(this.mappedData, delData => delData.documentRepositoryId === data.documentRepositoryId);
+    console.log(this.mappedData.length);
   }
-  openDocumentESign(value,state)
-  {
-    this.subInjectService.rightSliderData(state)
-    this.eventService.sliderData(value)
+
+  openDocumentESign(value, state) {
+    this.subInjectService.rightSliderData(state);
+    this.eventService.sliderData(value);
   }
-  getDocumentsDesignData(data){
-    this.documentDesign=data;
+
+  getDocumentsDesignData(data) {
+    this.documentDesign = data;
   }
-  changeDisplay(value)
-  {
-    this.documentDesign=value;
+
+  changeDisplay(value) {
+    this.documentDesign = value;
   }
-  getplanDocumentData()
-  {
-    let obj={
-      'advisorId':2735,
-      'planId':10
-    }
+
+  getplanDocumentData() {
+    const obj = {
+      advisorId: 2735,
+      planId: 10
+    };
     this.subService.getPlanDocumentsData(obj).subscribe(
-      data=>this.getplanDocumentDataResponse(data)
-    )
+      data => this.getplanDocumentDataResponse(data)
+    );
   }
-  getplanDocumentDataResponse(data)
-  {
-    this.planDocumentData=data;
+
+  getplanDocumentDataResponse(data) {
+    this.planDocumentData = data;
   }
-  deleteModal(value)
-  {
-    let dialogData = {
-      data:value,
+
+  deleteModal(value) {
+    const dialogData = {
+      data: value,
       header: 'DELETE',
-      body:'Are you sure you want to delete the document?',
-      body2:'This cannot be undone',
-      btnYes:'CANCEL',
-      btnNo:'DELETE'
-    }
+      body: 'Are you sure you want to delete the document?',
+      body2: 'This cannot be undone',
+      btnYes: 'CANCEL',
+      btnNo: 'DELETE'
+    };
 
     const dialogRef = this.dialog.open(ConfirmDialogComponent, {
-       width: '400px',
-       data: dialogData,
-       autoFocus:false,
+      width: '400px',
+      data: dialogData,
+      autoFocus: false,
 
     });
-  
+
     dialogRef.afterClosed().subscribe(result => {
-  
+
     });
-  
+
   }
-  savePlanMapToDocument()
-  {
-    let obj=[];
+
+  savePlanMapToDocument() {
+    const obj = [];
     this.mappedData.forEach(element => {
-       let data={
-        "advisorId": 2735,
-        "documentRepositoryId":element.documentRepositoryId,
-        "planId": 10
-       }
-       obj.push(data)
+      const data = {
+        advisorId: 2735,
+        documentRepositoryId: element.documentRepositoryId,
+        planId: 10
+      };
+      obj.push(data);
     });
     this.subService.mapDocumentsToPlanData(obj).subscribe(
-      data=>console.log(data)
-    )
+      data => console.log(data)
+    );
   }
-  displayedColumns: string[] = ['checkbox','document','plan','service', 'date', 'sdate','cdate','status','icons'];
-  dataSource = ELEMENT_DATA;
 }
