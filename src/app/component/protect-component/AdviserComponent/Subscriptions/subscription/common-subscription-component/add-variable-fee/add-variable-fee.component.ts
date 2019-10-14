3e7d8065d12371840b25c12569ab781653efec2a
Gaurav <gaurav@futurewise.co.in>
2019-10-14 16:13:52 +0530

    ng build --prod  issues resolved.
    Signed-off-by: Gaurav <gaurav@futurewise.co.in>

--- src/app/component/protect-component/AdviserComponent/Subscriptions/subscription/common-subscription-component/add-variable-fee/add-variable-fee.component.ts
+++ src/app/component/protect-component/AdviserComponent/Subscriptions/subscription/common-subscription-component/add-variable-fee/add-variable-fee.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { SubscriptionInject } from '../../../subscription-inject.service';
-import { FormBuilder, Validators } from '@angular/forms';
-import { SubscriptionService } from '../../../subscription.service';
-import { EnumServiceService } from '../../enum-service.service';
+import {Component, OnInit} from '@angular/core';
+import {SubscriptionInject} from '../../../subscription-inject.service';
+import {FormBuilder, Validators} from '@angular/forms';
+import {SubscriptionService} from '../../../subscription.service';
+import {EnumServiceService} from '../../enum-service.service';
 
 @Component({
   selector: 'app-add-variable-fee',
@@ -11,11 +11,12 @@ import { EnumServiceService } from '../../enum-service.service';
 })
 export class AddVariableFeeComponent implements OnInit {
 
-  constructor(public subInjectService: SubscriptionInject, private fb: FormBuilder, private subService: SubscriptionService,private enumService:EnumServiceService) {
+  constructor(public subInjectService: SubscriptionInject, private fb: FormBuilder, private subService: SubscriptionService, private enumService: EnumServiceService) {
     this.subInjectService.rightSideBarData.subscribe(
       data => this.getFeeFormData(data)
     )
   }
+
   isServiceValid;
   isCodeValid;
   idDesValid;
@@ -24,21 +25,25 @@ export class AddVariableFeeComponent implements OnInit {
   isRegularFees;
   isDirectFees;
   otherAssetData;
+
   ngOnInit() {
     this.setValidation(false)
     // this.otherAssetData=this.enumService.getOtherAssetData();
   }
+
   setValidation(flag) {
     this.isServiceValid = flag
     this.isCodeValid = flag
     this.idDesValid = flag
     this.isBillValid = flag
-    this.isDirectFees=flag
-    this.isRegularFees=flag
+    this.isDirectFees = flag
+    this.isRegularFees = flag
   }
+
   getFormControl() {
     return this.variableFeeData.controls;
   }
+
   getFeeFormData(data) {
     this.variableFeeData = this.fb.group({
       billingNature: [1],
@@ -62,43 +67,38 @@ export class AddVariableFeeComponent implements OnInit {
     this.getFormControl().code.maxLength = 10;
     this.getFormControl().description.maxLength = 160;
   }
+
   Close(state) {
     this.subInjectService.rightSliderData(state)
     this.setValidation(false)
     this.variableFeeData.reset();
   }
+
   closeTab(state, value) {
     console.log(state)
     this.subInjectService.rightSliderData(state)
     this.subInjectService.closeSlider(value)
   }
+
   saveVariableFeeData(feeType) {
-  
+
     if (this.variableFeeData.controls.serviceName.invalid) {
       this.isServiceValid = true;
       return;
-    }
-    else if (this.variableFeeData.controls.code.invalid) {
+    } else if (this.variableFeeData.controls.code.invalid) {
       this.isCodeValid = true;
       return;
-    }
-    else if (this.variableFeeData.controls.description.invalid) {
+    } else if (this.variableFeeData.controls.description.invalid) {
       this.idDesValid = true;
       return;
-    }
-    else if (this.variableFeeData.controls.billEvery.invalid) {
+    } else if (this.variableFeeData.controls.billEvery.invalid) {
       this.isBillValid = true;
       return;
-    }
-    else if(this.variableFeeData.controls.directFees.invalid)
-    {
-     this.isDirectFees=true;
-    }
-    else if(this.variableFeeData.controls.regularFees.invalid)
-    {
-      this.isRegularFees=true;
-    }
-    else {
+    } else if (this.variableFeeData.controls.directFees.invalid) {
+      this.isDirectFees = true;
+    } else if (this.variableFeeData.controls.regularFees.invalid) {
+      this.isRegularFees = true;
+    } else {
       let obj = {
         "advisorId": 12345,
         "description": this.variableFeeData.controls.description.value,
@@ -121,8 +121,8 @@ export class AddVariableFeeComponent implements OnInit {
               "pricing": 0
             }, {
               "assetClassId": 2,
-              "debtAllocation":this.variableFeeData.controls.regularFees.controls.dept.value,
-              "equityAllocation":this.variableFeeData.controls.regularFees.controls.dept.value,
+              "debtAllocation": this.variableFeeData.controls.regularFees.controls.dept.value,
+              "equityAllocation": this.variableFeeData.controls.regularFees.controls.dept.value,
               "liquidAllocation": this.variableFeeData.controls.regularFees.controls.dept.value,
               // "otherAssets": [
               //   1, 2
