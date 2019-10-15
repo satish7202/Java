2d19b8c7022deff877a7aa3895ceef8330f5e86e
Gaurav <gaurav@futurewise.co.in>
2019-10-15 13:53:55 +0530

    Invoice Selection count issue resolved GD COMMIT
    Signed-off-by: Gaurav <gaurav@futurewise.co.in>

--- src/app/component/protect-component/AdviserComponent/Subscriptions/subscription/common-subscription-component/invoice/invoice.component.ts
+++ src/app/component/protect-component/AdviserComponent/Subscriptions/subscription/common-subscription-component/invoice/invoice.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, Input ,Output, EventEmitter} from '@angular/core';
-import { SubscriptionInject } from '../../../subscription-inject.service';
-import { FormBuilder ,Validators } from '@angular/forms';
+import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import {SubscriptionInject} from '../../../subscription-inject.service';
+import {FormBuilder, Validators} from '@angular/forms';
 
 export interface PeriodicElement {
   document: string;
@@ -8,11 +8,19 @@ export interface PeriodicElement {
   date: string;
   sdate: string;
   cdate: string;
-  status:string;
- } 
- const ELEMENT_DATA: PeriodicElement[] = [
-  {document: "Scope of work", plan: 'Starter plan', date: "25/08/2019", sdate: '25/08/2019',cdate: '25/08/2019',status:'READY TO SEND'},
-  
+  status: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {
+    document: 'Scope of work',
+    plan: 'Starter plan',
+    date: '25/08/2019',
+    sdate: '25/08/2019',
+    cdate: '25/08/2019',
+    status: 'READY TO SEND'
+  },
+
 ];
 
 @Component({
@@ -21,6 +29,15 @@ export interface PeriodicElement {
   styleUrls: ['./invoice.component.scss']
 })
 export class InvoiceComponent implements OnInit {
+
+  constructor(public subInjectService: SubscriptionInject, private fb: FormBuilder) {
+    this.dataSub = this.subInjectService.singleProfileData.subscribe(
+      data => this.getInvoiceData(data)
+    );
+    this.subInjectService.rightSideBarData.subscribe(
+      data => this.getRecordPayment(data)
+    );
+  }
   dataSub: any;
   storeData;
   showRecord: any;
@@ -32,120 +49,107 @@ export class InvoiceComponent implements OnInit {
   istdsValid: boolean;
   ismodeValid: boolean;
 
-  constructor(public subInjectService:SubscriptionInject,private fb:FormBuilder) {
-    this.dataSub = this.subInjectService.singleProfileData.subscribe(
-      data=>this.getInvoiceData(data)
-    );
-    this.subInjectService.rightSideBarData.subscribe(
-      data=>this.getRecordPayment(data)
-    )
-   }
   @Input() invoiceData;
   @Input() invoiceInSub;
   @Input() clientData;
   @Output() valueChange = new EventEmitter();
+
+  @Input() invoiceTab;
+  rPayment;
+
+  displayedColumns: string[] = ['checkbox', 'document', 'plan', 'date', 'sdate', 'cdate', 'status', 'icons'];
+  dataSource = ELEMENT_DATA;
+
   ngOnInit() {
-  console.log('this.invoiceSubscription',this.invoiceInSub);
-  this.showRecord=false;
-  this.showEdit=false;
+    console.log('this.invoiceSubscription', this.invoiceInSub);
+    this.showRecord = false;
+    this.showEdit = false;
   }
- @Input() invoiceTab;
- rPayment;
- getRecordPayment(data)
- {
-  console.log("payee data", data);
-  this.rPayment = this.fb.group({
-    amountReceive: [data.amountReceive,[Validators.required]],
-    charges: [data.charges,[Validators.required]],
-    tds: [data.tds,[Validators.required]],
-    paymentDate: [data.paymentDate],
-    paymentMode: [data.paymentMode,[Validators.required]],
-    gstTreatment: [data.gstTreatment],
-    notes: [data.notes]
-  }) 
-  this.getFormControl().amountReceive.maxLength = 10;
+
+  getRecordPayment(data) {
+    console.log('payee data', data);
+    this.rPayment = this.fb.group({
+      amountReceive: [data.amountReceive, [Validators.required]],
+      charges: [data.charges, [Validators.required]],
+      tds: [data.tds, [Validators.required]],
+      paymentDate: [data.paymentDate],
+      paymentMode: [data.paymentMode, [Validators.required]],
+      gstTreatment: [data.gstTreatment],
+      notes: [data.notes]
+    });
+    this.getFormControl().amountReceive.maxLength = 10;
     this.getFormControl().charges.maxLength = 10;
     this.getFormControl().tds.maxLength = 10;
-    this.getFormControl().paymentMode.maxLength =10;
-    this.getFormControl().notes.maxLength =10;
+    this.getFormControl().paymentMode.maxLength = 10;
+    this.getFormControl().notes.maxLength = 10;
 
- }
- getFormControl()
-  {
+  }
+
+  getFormControl() {
     return this.rPayment.controls;
   }
-  saveFormData(state)
-  {
-    if(this.rPayment.controls.amountReceive.invalid)
-    {
-      this.isamountValid=true;
-     return;
-    }
-    else if(this.rPayment.controls.charges.invalid)
-    {
-      this.ischargeValid=true;
+
+  saveFormData(state) {
+    if (this.rPayment.controls.amountReceive.invalid) {
+      this.isamountValid = true;
       return;
-    }
-    else if(this.rPayment.controls.tds.invalid)
-    {
-      this.istdsValid=true
+    } else if (this.rPayment.controls.charges.invalid) {
+      this.ischargeValid = true;
       return;
+    } else if (this.rPayment.controls.tds.invalid) {
+      this.istdsValid = true;
+      return;
+    } else if (this.rPayment.controls.paymentMode.invalid) {
+      this.ismodeValid = true;
+      return;
+    } else {
+      const obj = {
+        advisorId: 12345,
+        amountReceieve: this.rPayment.controls.amountReceive.value,
+        chargeIfAny: this.rPayment.controls.charges.value,
+        TDS: this.rPayment.controls.tds.value,
+        paymentDate: this.rPayment.controls.paymentDate.value,
+        paymentMode: this.rPayment.controls.paymentMode.value,
+        gstTreatment: this.rPayment.controls.gstTreatment.value,
+        notes: this.rPayment.controls.notes.value,
+      };
+      console.log('jifsdfoisd', obj);
     }
-    else if(this.rPayment.controls.paymentMode.invalid)
-    {
-       this.ismodeValid=true;
-       return;
-    }
-    else{
-     let obj = {
-       "advisorId": 12345,
-       "amountReceieve":this.rPayment.controls.amountReceive.value,
-       "chargeIfAny": this.rPayment.controls.charges.value,
-       "TDS": this.rPayment.controls.tds.value ,
-       "paymentDate":this.rPayment.controls.paymentDate.value,
-       "paymentMode":this.rPayment.controls.paymentMode.value,
-       "gstTreatment":this.rPayment.controls.gstTreatment.value,
-       "notes":this.rPayment.controls.notes.value,
-     }
-      console.log("jifsdfoisd",obj)
-    } 
     this.cancel();
   }
- getInvoiceData(data)
-   {
-    this.storeData = data
-   }
- recordPayment()
- {
-  this.showRecord=true;
- }
- cancel()
- {
-   this.showRecord=false;
- }
- passInvoice(data)
- {
-  console.log(data);
-  this.storeData=data;
- }
- editInvoice()
- {
-  this.showEdit=true;
- }
- closeEditInv()
- {
-  this.showEdit=false;
- }
-  Close(state)
-  { 
-    if(this.showRecord==true){
-      this.showRecord=false
-    }else{
-      (this.invoiceTab=='invoiceUpperSlider')?this.subInjectService.rightSliderData(state):this.subInjectService.rightSideData(state)
+
+  getInvoiceData(data) {
+    this.storeData = data;
+  }
+
+  recordPayment() {
+    this.showRecord = true;
+  }
+
+  cancel() {
+    this.showRecord = false;
+  }
+
+  passInvoice(data) {
+    console.log(data);
+    this.storeData = data;
+  }
+
+  editInvoice() {
+    this.showEdit = true;
+  }
+
+  closeEditInv() {
+    this.showEdit = false;
+  }
+
+  Close(state) {
+    if (this.showRecord == true) {
+      this.showRecord = false;
+    } else {
+      (this.invoiceTab == 'invoiceUpperSlider') ? this.subInjectService.rightSliderData(state) : this.subInjectService.rightSideData(state);
       this.valueChange.emit(this.invoiceInSub);
     }
-    
+
   }
-  displayedColumns: string[] = ['checkbox','document','plan', 'date', 'sdate','cdate','status','icons'];
-  dataSource = ELEMENT_DATA;
 }
--- src/app/component/protect-component/AdviserComponent/Subscriptions/subscription/invoices-subscription/invoices-subscription.component.ts
+++ src/app/component/protect-component/AdviserComponent/Subscriptions/subscription/invoices-subscription/invoices-subscription.component.ts
@@ -1,22 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { SubscriptionService } from '../../subscription.service';
-import { SubscriptionInject } from '../../subscription-inject.service';
-import { EventService } from 'src/app/Data-service/event.service';
-import { ValueConverter } from '@angular/compiler/src/render3/view/template';
-import { ConfirmDialogComponent } from 'src/app/component/protect-component/common-component/confirm-dialog/confirm-dialog.component';
-import { MatDialog } from '@angular/material';
+import {Component, OnInit} from '@angular/core';
+import {SubscriptionService} from '../../subscription.service';
+import {SubscriptionInject} from '../../subscription-inject.service';
+import {EventService} from 'src/app/Data-service/event.service';
+import {ValueConverter} from '@angular/compiler/src/render3/view/template';
+import {ConfirmDialogComponent} from 'src/app/component/protect-component/common-component/confirm-dialog/confirm-dialog.component';
+import {MatDialog} from '@angular/material';
 
 export interface PeriodicElement {
-  date:string;
-  invoicenum:string;
+  date: string;
+  invoicenum: string;
   name: string;
   email: string;
-  status:string;
+  status: string;
   duedate: string;
   amt: string;
-  balance:string;
-  
-} 
+  balance: string;
+
+}
 
 @Component({
   selector: 'app-invoices-subscription',
@@ -24,45 +24,51 @@ export interface PeriodicElement {
   styleUrls: ['./invoices-subscription.component.scss']
 })
 export class InvoicesSubscriptionComponent implements OnInit {
+
+  constructor(public dialog: MatDialog, public subInjectService: SubscriptionInject,
+              private eventService: EventService, public subscription: SubscriptionService) {
+    this.ngOnInit();
+  }
+
   dataSource: any;
   subscriptionValue: any;
   invoiceSub: any;
   invoiceSubscription: string;
   invoiceClientData: any;
   dataCount: any;
-  constructor(public dialog:MatDialog,public subInjectService:SubscriptionInject, private eventService:EventService,public subscription:SubscriptionService) {
-    this.ngOnInit(); 
-  }
-  
+
   showPdfInvoice;
   singleInvoiceData;
-  showLoader=true;
+  showLoader = true;
+
+  displayedColumns: string[] = ['checkbox', 'date', 'invoicenum', 'name', 'status', 'email', 'duedate', 'amt', 'balance'];
+
   ngOnInit() {
-   this.getInvoiceSubData();
-   this.invoiceSubscription='false';
-   this.dataCount=0;
+    this.getInvoiceSubData();
+    this.invoiceSubscription = 'false';
+    this.dataCount = 0;
   }
-  getInvoiceSubData()
-  {
-    let obj={
-     'id':2735, //pass here advisor id for Invoice advisor 
-     'module':1
-    }
+
+  getInvoiceSubData() {
+    const obj = {
+      id: 2735, // pass here advisor id for Invoice advisor
+      module: 1
+    };
 
     this.subscription.getInvoices(obj).subscribe(
-      data =>this.getInvoiceResponseData(data)
-    )
+      data => this.getInvoiceResponseData(data)
+    );
   }
-  getInvoiceResponseData(data)
-  {
-    console.log(data)
-    const ELEMENT_DATA=data;
-    this.invoiceClientData=data;
+
+  getInvoiceResponseData(data) {
+    console.log(data);
+    const ELEMENT_DATA = data;
+    this.invoiceClientData = data;
     ELEMENT_DATA.forEach(item => item.selected = false);
-   this.dataSource=ELEMENT_DATA;
-   this.showLoader=false;
+    this.dataSource = ELEMENT_DATA;
+    this.showLoader = false;
   }
-  displayedColumns: string[] = ['checkbox','date','invoicenum','name','status','email', 'duedate', 'amt','balance'];
+
   // showInvoicePdf(value)
   // {
   //  this.subscription.getSingleInvoiceData(value.id).subscribe(
@@ -70,52 +76,57 @@ export class InvoicesSubscriptionComponent implements OnInit {
   //  )
   // this.showPdfInvoice=true;
   // }
-  openInvoice(data,value,state)
-  {
-    this.invoiceSub=value;
-    this.invoiceSubscription='true';
+  openInvoice(data, value, state) {
+    this.invoiceSub = value;
+    this.invoiceSubscription = 'true';
 //     this.eventService.sidebarData(value)
 // this.subscriptionValue = value
 // this.subInjectService.rightSideData(state);
-this.subInjectService.addSingleProfile(data)
+    this.subInjectService.addSingleProfile(data);
 
   }
-  getSingleResponseInvoicePdf(data)
-  {
-    this.singleInvoiceData=data
+
+  getSingleResponseInvoicePdf(data) {
+    this.singleInvoiceData = data;
   }
-  selectAll(event){
+
+  selectAll(event) {
     // const checked = event.target.checked;
     // this.dataSource.forEach(item => item.selected = 'checked');
+    this.dataCount = 0;
     this.dataSource.forEach(item => {
-    //   if(item.selected==false) 
-    //   {
-    //     item.selected = true;
-    //     this.dataCount++;
-    //   }else{
-    //     item.selected = false;
-    //     this.dataCount--;
-    //   }
-    // });
-    item.selected = event.checked;
-    // if(item.dataCountd>=1){
-    //   this.dataCount=1
-    // }else{
-    //   this.dataCount++
-    // }
+      //   if(item.selected==false)
+      //   {
+      //     item.selected = true;
+      //     this.dataCount++;
+      //   }else{
+      //     item.selected = false;
+      //     this.dataCount--;
+      //   }
+      // });
+      item.selected = event.checked;
+      if (item.selected) {
+        this.dataCount++;
+      }
+      // if(item.dataCountd>=1){
+      //   this.dataCount=1
+      // }else{
+      //   this.dataCount++
+      // }
     });
     // if(item.selected=="true"){
     //   this.dataCount++;
     // }else{
     //   this.dataCount--;
     // }
-    
+
   }
-  changeSelect(data)
-  {
-    this.dataCount=0;
+
+  changeSelect(data) {
+    this.dataCount = 0;
     this.dataSource.forEach(item => {
-      if(item.selected==true){
+      console.log('item item ', item);
+      if (item.selected) {
         this.dataCount++;
       }
     });
@@ -130,11 +141,12 @@ this.subInjectService.addSingleProfile(data)
     //   data.dataCountd =this.dataCount;
     // }
   }
-  display(data)
-  {
+
+  display(data) {
     console.log(data);
     this.ngOnInit();
   }
+
   deleteModal(value) {
     const dialogData = {
       data: value,
@@ -149,8 +161,8 @@ this.subInjectService.addSingleProfile(data)
       negativeMethod: () => {
         console.log('2222222222222222222222222222222222222');
       }
-    }
-    console.log(dialogData+"11111111111111");
+    };
+    console.log(dialogData + '11111111111111');
 
     const dialogRef = this.dialog.open(ConfirmDialogComponent, {
       width: '400px',
