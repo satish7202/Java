e2569406b0711ddbe843a963b50189c765649c09
sunny-ui <sunny@futurewise.co.in>
2019-10-22 18:25:20 +0530

    css changes in subs

--- src/app/component/protect-component/AdviserComponent/Subscriptions/subscription/common-subscription-component/invoice/invoice.component.ts
+++ src/app/component/protect-component/AdviserComponent/Subscriptions/subscription/common-subscription-component/invoice/invoice.component.ts
@@ -11,8 +11,30 @@ export interface PeriodicElement {
   sdate: string;
   cdate: string;
   status: string;
+  
 }
-
+export class TableStickyHeaderExample {
+  displayedColumns1 = ['position', 'name', 'weight', 'symbol'];
+  dataSource1 = ELEMENT_DATA1;
+}
+export interface PeriodicElement1 {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+const ELEMENT_DATA1: PeriodicElement1[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
 const ELEMENT_DATA: PeriodicElement[] = [
   {
     document: 'Scope of work',
@@ -301,4 +323,5 @@ export class InvoiceComponent implements OnInit {
   {
     console.log(this.editPayment)
   }
+  
 }
