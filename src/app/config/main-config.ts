b54b1b09b2381fe938031baa660aa80f91fbcd24
satish7202 <92143617+satish7202@users.noreply.github.com>
2021-10-29 16:49:32 +0530

    Delete src directory

--- src/app/config/main-config.ts
+++
@@ -1,5 +0,0 @@
-
- import { environment } from '../../environments/environment';
- export const apiConfig ={
-     MAIN_URL:environment.APIEndpoint
-}
--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  // APIEndpoint: 'http://localhost:8080/futurewise/api/v1/web/'
-
-  APIEndpoint: 'http://dev.ifanow.in:8080/futurewise/api/v1/web/'
-  // APIEndpoint:'http://dev.ifanow.in:8080/'
-};
